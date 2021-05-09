11671d7e847fc687565dc653ee82d3ef0745a5aa
1750402901 <1750402901@qq.com>
2021-05-09 21:24:33 +0800

    启动类

--- src/main/java/com/lxd/BlogLxdApplication.java
+++ src/main/java/com/lxd/BlogLxdApplication.java
@@ -3,6 +3,7 @@ package com.lxd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动类
 @SpringBootApplication
 public class BlogLxdApplication {
 
